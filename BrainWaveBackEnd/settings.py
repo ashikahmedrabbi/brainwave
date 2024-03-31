0597048243a78375c6b6dd766bfa0579117a7191
unknown <ashikahmedrabbi@gmail.com>
2024-03-31 12:59:27 +0600

    ready for go

--- BrainWaveBackEnd/settings.py
+++ BrainWaveBackEnd/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-kpd*c(eyapln3rs8g9u2ju+w1$z-^#3t@4xwb&i64kzlt@g)6e
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://smart-care.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://brainwave-zc9o.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 ROOT_URLCONF = 'smart_care.urls'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://smart-care.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://brainwave-zc9o.onrender.com']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
