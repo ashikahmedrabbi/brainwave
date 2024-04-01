c34e335088341bebee35e182884190006aadd2a0
unknown <ashikahmedrabbi@gmail.com>
2024-04-01 16:24:02 +0600

    updated

--- service/models.py
+++ service/models.py
@@ -4,3 +4,6 @@ class Service(models.Model):
     name = models.CharField(max_length = 20)
     description = models.TextField()
     image = models.ImageField(upload_to="service/images/")
+
+    def __str__(self):
+        return self.name
